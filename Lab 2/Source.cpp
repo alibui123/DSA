55282aaa044b667c524fddd663f8b03f03b2ab91
alibui123 <ali0909abbasi@gmail.com>
2025-02-23 23:10:56 +0500

    Add files via upload

---
+++ Lab 2/Source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "stack.h"
+using namespace std;
+
+/*int main() {
+    Stack s;
+    s.push(10);
+    s.push(120);
+    s.push(1);
+
+
+    cout << "Top element is: " << s.peek() << endl;
+
+    return 0;
+}*/
+/*int main()
+{
+    char c;
+    Stack s;
+    string input;
+    cin >> input;
+    s.reverse(input);
+
+
+}*/
+/*int main() {
+    Stack s;
+    string input;
+    cin >> input;
+    s.validity(input);
+}*/
+
+
