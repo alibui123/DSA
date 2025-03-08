b5a2fc356d7ad952f06ae0e866d32e862ea3a11f
alibui123 <ali0909abbasi@gmail.com>
2025-03-08 15:36:32 +0500

    Add files via upload

---
+++ lab 4/que.h
@@ -0,0 +1,17 @@
+//#define maxque 50
+//template<class itemtype>
+//class que
+//{
+//private:
+//	itemtype items[maxque];
+//	int front, rear, count;
+//public:
+//	que();
+//	int isempty();
+//	int isfull();
+//	void insertrear(itemtype newitem);
+//	void removerear(itemtype& item);
+//	void insertfront(itemtype newitem);
+//	void removefront(itemtype& item);
+//};
+//
